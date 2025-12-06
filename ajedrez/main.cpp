6ee1a0efea95a0a7372b7f3c8e51c8b4857ea21d
DrRon <DoctorRon64@users.noreply.github.com>
2025-12-06 19:12:55 +0100

    [ADDED] Player Input

--- ajedrez/main.cpp
+++ ajedrez/main.cpp
@@ -1,14 +1,37 @@
 #include "Board.h"
-#include "Includes.h"
-
-struct vector2 {
-	short x, y;
-};
+#include <iostream>
 
 int main() {
 	Board* board = new Board();
 	init_board(board);
-	print_board(board, 8);
+
+	bool game_over = false;
+	bool white_turn = true;
+
+	while(!game_over) {
+		print_board(board, 8);
+		std::cout << (white_turn ? "White" : "Black") << " to move." << std::endl;
+
+		Vector2 from = read_position();
+
+		if(!in_bounds(from.x, from.y) || !has_piece(board, from.x, from.y)) {
+			std::cout << "Invalid piece.\n";
+			break;
+		}
+	}
 
 	return 0;
 }
+
+//TODO
+//piece
+//pos
+//moveDir (possible move positions in vectors)
+//extra variables for other conditions
+
+//TODO
+// Prompt player to SELECT A piece (which pos)
+// Check if what player typed is a position that contains a piece in the board
+// Prompt player  for a position to move the piece in
+// Check if that positon its a correct position for the selected piece we previously selected.
+// if position is correct move piece & if position contains a piece, if piece moves towards the piece, delete piece from the board and overwrite the piece on top of it
