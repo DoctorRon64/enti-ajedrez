c3f7f6844ac52bc1d98a18b2a2c4422233a3a3b2
DrRon <DoctorRon64@users.noreply.github.com>
2025-12-06 19:07:00 +0100

    [ADDED] pieces

---
+++ ajedrez/Piece.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <ctype.h>
+
+enum PieceType {
+	PAWN,
+	ROOK,
+	KNIGHT,
+	BISHOP,
+	QUEEN,
+	KING,
+	NONE
+};
+
+struct Piece {
+	char type;
+	bool isWhite;
+};
+
+PieceType get_piece_type(char p) {
+	switch(tolower(p)) {
+		case 'p': return PAWN;
+		case 'r': return ROOK;
+		case 'n': return KNIGHT;
+		case 'b': return BISHOP;
+		case 'q': return QUEEN;
+		case 'k': return KING;
+		default: return NONE;
+	}
+}
+
+bool is_white(char p) {
+	return p >= 'A' && p <= 'Z';
+}
+
+bool is_black(char p) {
+	return p >= 'a' && p <= 'z';
+}
---
+++ ajedrez/Vector2.h
@@ -0,0 +1,5 @@
+#pragma once
+struct Vector2 {
+	short x;
+	short y;
+};
