b8d9cfb69d9158fcd7a4bbc6302288de26823b2c
DrRon <DoctorRon64@users.noreply.github.com>
2025-12-12 10:28:36 +0100

    [MOVED] Splitted up functions to cpp file

--- ajedrez/Piece.h
+++ ajedrez/Piece.h
@@ -18,22 +18,6 @@ struct Piece {
 	Vector2 pos;
 };
 
-PieceType get_piece_type(char p) {
-	switch(tolower(p)) {
-		case 'p': return PAWN;
-		case 'r': return ROOK;
-		case 'n': return KNIGHT;
-		case 'b': return BISHOP;
-		case 'q': return QUEEN;
-		case 'k': return KING;
-		default: return NONE;
-	}
-}
-
-bool is_white(char p) {
-	return p >= 'A' && p <= 'Z';
-}
-
-bool is_black(char p) {
-	return p >= 'a' && p <= 'z';
-}
+PieceType get_piece_type(char p);
+bool is_white(char p);
+bool is_black(char p);
