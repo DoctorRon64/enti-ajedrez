bdc09122e7fd9b46394554eeb753df904e172958
DrRon <DoctorRon64@users.noreply.github.com>
2025-12-19 13:45:02 +0100

    [FIXED] correct piece selection

--- ajedrez/main.cpp
+++ ajedrez/main.cpp
@@ -5,43 +5,47 @@
 #include <iostream>
 
 int main() {
-	Board* board = new Board();
-	init_board(board);
+	Board board;
+	init_board(&board);
 
 	bool game_over = false;
 	bool white_turn = true;
 
 	while(!game_over) {
 		system("cls");
-		print_board(board, 8);
-		std::cout << (white_turn ? "White" : "Black") << " to move." << std::endl;
+		print_board(&board, BOARD_SIZE);
+		std::cout << (white_turn ? "White" : "Black") << " to move.\n";
 
 		Vector2 from = read_position();
-		if(!in_bounds(from.x, from.y) || !has_piece(board, from.x, from.y)) {
+		if(!in_bounds(from.x, from.y) || !has_piece(&board, from.x, from.y)) {
 			std::cout << "Invalid piece.\n";
-
-			throw std::runtime_error("Division by zero error");
 			continue;
 		}
-		else {
-			char piece = board->cells[from.x][from.y];
-			if(white_turn && is_black(piece) || !white_turn && is_white(piece)) {
-				std::cout << "That's not your piece!\n";
-				continue;
+
+		char piece = board.cells[from.x][from.y];
+		if((white_turn && is_black(piece)) || (!white_turn && is_white(piece))) {
+			std::cout << "That's not your piece!\n";
+			continue;
+		}
+
+		Vector2 to = read_position();
+		if(!in_bounds(to.x, to.y)) {
+			std::cout << "Destination out of bounds!\n";
+			continue;
+		}
+
+		if(is_valid_move(&board, from, to)) {
+			move_piece(&board, from, to);
+
+			if(!is_king_alive(&board)) {
+				game_over = true;
+				std::cout << (white_turn ? "White" : "Black") << " wins!\n";
 			}
-			Vector2 to = read_position();
-			if(!in_bounds(to.x, to.y)) {
-				std::cout << "Destination out of bounds!\n";
-				continue;
-			}
-			else if(is_valid_move(board, from, to)) {
-				move_piece(board, from, to);
+			else {
+				white_turn = !white_turn;
 			}
 		}
 	}
 
 	return 0;
 }
-
-// Check if that positon its a correct position for the selected piece we previously selected.
-// if position is correct move piece & if position contains a piece, if piece moves towards the piece, delete piece from the board and overwrite the piece on top of it
